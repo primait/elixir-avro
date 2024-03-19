3a4f1dd182e9a648854e4731086c1d09fd017424
Simone Cottini <cottini.simone@gmail.com>
2024-03-19 09:19:37 +0100

    Cloned code from internal repo

---
+++ lib/elixir_avro/trainer_level.ex
@@ -0,0 +1,11 @@
+defmodule TrainerLevel do
+  @moduledoc """
+  DO NOT EDIT MANUALLY: This module was automatically generated from an AVRO schema.
+
+  Trainer certified level.
+
+  """
+  use ElixirAvro.ElixirEnum
+
+  @values ["BEGINNER", "INTERMEDIATE", "ADVANCE"]
+end
