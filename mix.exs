f42184fb3f49869d41fc1de869b52eaedefb85ec
Mikhail Krivushin <mikhail@dice.fm>
2024-07-24 12:11:26 +0100

    Add EEx to extra_applications

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule ElixirAvro.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :eex],
       mod: {ElixirAvro.Application, []}
     ]
   end
